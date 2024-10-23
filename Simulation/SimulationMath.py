e32d5daab378e21b0052c6aba9858ecccc07d320
azinho <89921317+azinhoxx@users.noreply.github.com>
2024-10-23 15:07:27 +0300

    rename functions

--- Simulation/SimulationMath.py
+++ Simulation/SimulationMath.py
@@ -8,8 +8,8 @@ def calculateNaturalFrequency(k: float, mass: float) -> float:
 def calculateHarmonicOscillation(time: pd.Timedelta, frequency: float, amplitude: float) -> float:
     return amplitude * math.sin(frequency * time.total_seconds())
 
-def calculateHarmonicOscillationSpeed(time: pd.Timedelta, frequency: float, amplitude: float) -> float:
+def calculateHarmonicOscillationVelocity(time: pd.Timedelta, frequency: float, amplitude: float) -> float:
     return amplitude * frequency * math.cos(frequency * time.total_seconds())
 
-def calculateHarmonicOscillationVelocity(time: pd.Timedelta, frequency: float, amplitude: float) -> float:
+def calculateHarmonicOscillationAcceleration(time: pd.Timedelta, frequency: float, amplitude: float) -> float:
     return amplitude * frequency ** 2 * math.cos(frequency * time.total_seconds() + math.pi / 2)
