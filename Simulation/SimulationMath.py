e92a2bfc97b367ed9417c404a56188a9bb142d9f
azinho <89921317+azinhoxx@users.noreply.github.com>
2024-10-23 15:00:24 +0300

    change math functions

--- Simulation/SimulationMath.py
+++ Simulation/SimulationMath.py
@@ -2,12 +2,14 @@ import math
 
 import pandas as pd
 
-
 def calculateNaturalFrequency(k: float, mass: float) -> float:
     return math.sqrt(k / mass) * 2 * math.pi
 
 def calculateHarmonicOscillation(time: pd.Timedelta, frequency: float, amplitude: float) -> float:
     return amplitude * math.sin(frequency * time.total_seconds())
 
+def calculateHarmonicOscillationSpeed(time: pd.Timedelta, frequency: float, amplitude: float) -> float:
+    return amplitude * frequency * math.cos(frequency * time.total_seconds())
+
 def calculateHarmonicOscillationVelocity(time: pd.Timedelta, frequency: float, amplitude: float) -> float:
-    return frequency * amplitude * math.cos(frequency * time.total_seconds())
+    return amplitude * frequency ** 2 * math.cos(frequency * time.total_seconds() + math.pi / 2)
