d50b44789f8e5c7bb5e2e17a07176381da4c4764
Timofei <timabelov08@gmail.com>
2024-10-28 22:19:51 +0300

    example of setForcedDisplacement

--- main.py
+++ main.py
@@ -14,5 +14,8 @@ if __name__ == "__main__":
     simulation.setForcedOscillation([
         OscillationInfo((-1,), offset / 3, calculateNaturalFrequency(mass=mass, k=k)),
     ])
+    mask = np.full(shape=(102,), fill_value=False, dtype=bool)
+    mask[50] = True
+    simulation.setForcedDisplacement(mask, offset)
     for i in range(0, 10000):
         simulation.iterate()
