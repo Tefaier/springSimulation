183f823350a17253ddb8dfbd318969c8e975a895
Timofei <timabelov08@gmail.com>
2024-10-30 19:16:32 +0300

    functions

--- Simulation/SimulationMath.py
+++ Simulation/SimulationMath.py
@@ -13,3 +13,12 @@ def calculateHarmonicOscillationVelocity(time: pd.Timedelta, frequency: float, a
 
 def calculateHarmonicOscillationAcceleration(time: pd.Timedelta, frequency: float, amplitude: float) -> float:
     return amplitude * frequency ** 2 * math.cos(frequency * time.total_seconds() + math.pi / 2)
+
+# viscosity - вязкость в Па*с
+# mass - масса шарика в кг
+# density - плотность материала шарика, кг/м^3, по умолчанию железо
+def getFrictionCoefficientMas(viscosity: float, mass: float, density: float = 7874) -> float:
+    return getFrictionCoefficientRad(viscosity, ((((mass / density) * 3) / 4) / math.pi) ** (1./3))
+
+def getFrictionCoefficientRad(viscosity: float, radius: float) -> float:
+    return 6 * math.pi * radius * viscosity
